9ad17bbed8c324b677e71a6f5c48e314464b5e30
Jayaraju bura <92459601+jayaraju-bura@users.noreply.github.com>
2023-08-14 00:53:06 +0530

    added method to display the contents of trie

--- Trie.java
+++ Trie.java
@@ -1,4 +1,4 @@
-
+// class which implements insert, search, remove and display operations of TRIE
 public class Trie {
     static final int ALPHABET_SIZE = 26;
     static class TrieNode {
@@ -46,6 +46,59 @@ public class Trie {
         
     }
     
+    static boolean isEmpty(TrieNode ptr){
+        for(int i=0 ; i < ALPHABET_SIZE; i++){
+            if(ptr.children[i] != null){
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    static TrieNode remove(TrieNode root, String key, int depth) {
+        if(root == null){
+            return root;
+        }
+        
+        if(depth == key.length()){
+            if(root.isEndOfWord){
+                root.isEndOfWord = false;
+            }
+            if(isEmpty(root)){
+            root = null;
+                
+            }
+            return root;
+        }
+        int index = key.charAt(depth) - 'a';
+        root.children[index] = remove(root.children[index], key, depth + 1);
+        
+        if(isEmpty(root) && root.isEndOfWord == false)
+        {
+            root = null;
+        }
+        
+        return root;
+    }
+    
+    static void display(TrieNode ptr, char[] str, int depth){
+        if(ptr.isEndOfWord){
+            
+            for(int k=depth; k < str.length; k++){
+                str[k] = 0;
+            }
+            System.out.println(str);
+        }
+        
+        for(int i=0; i < ALPHABET_SIZE; i++){
+            if(ptr.children[i] != null){
+                str[depth] = (char) (i + 'a');
+                display(ptr.children[i], str, depth + 1);
+            }
+            
+        }
+    }
+    
     public static void main(String args[]) {
         String keys[] = {"like", "a", "virgin", "secrets", "they",
                          "dont", "teach", "business", "at", "school"};
@@ -62,8 +115,8 @@ public class Trie {
       
         // Search for different keys
         if(search("teach") == true)
-            System.out.println("the --- " + output[1]);
-        else System.out.println("the --- " + output[0]);
+            System.out.println("teach --- " + output[1]);
+        else System.out.println("teach --- " + output[0]);
          
         if(search("these") == true)
             System.out.println("these --- " + output[1]);
@@ -74,13 +127,20 @@ public class Trie {
         else System.out.println("their --- " + output[0]);
          
         if(search("at") == true)
-            System.out.println("thaw --- " + output[1]);
-        else System.out.println("thaw --- " + output[0]);
+            System.out.println("at --- " + output[1]);
+        else System.out.println("at --- " + output[0]);
+        
+        remove(root, "at", 0);
+        if(search("at") == true)
+            System.out.println("at --- " + output[1]);
+        else System.out.println("at --- " + output[0]);
+        
+        char str[] = new char[100];
+        display(root, str, 0);
     }
     
     
 }
-
 
 // the --- Present in trie
 // these --- Not present in trie
